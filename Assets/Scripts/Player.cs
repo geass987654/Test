3d746db6598e40cde7a559046da47c08c2f4bca8
geass987654 <geass987654@gmail.com>
2023-09-14 23:42:59 +0800

    新增三角形和更新程式碼
    列印test

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,12 +7,6 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Debug.Log("test");
     }
 }
